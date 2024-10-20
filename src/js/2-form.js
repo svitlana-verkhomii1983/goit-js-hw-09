f9a255658ba7e1c3c1ae76f48c0c96bf16e281c2
Svitlana Verkhomii <svitlanaverkhomii@gmail.com>
2024-10-20 21:10:23 +0100

    add new style

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,34 +1,49 @@
-// Створення об'єкт formData
-const formData = { email: "", message: "" };
+// Ініціалізація об'єкту для збереження даних форми
+const formData = {
+    email: "",
+    message: ""
+};
 
-// Делегування подій для відстеження змін у формі та зберігання даних у локальне сховище
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', (event) => {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-});
+// Функція для збереження даних в локальне сховище
+const saveToLocalStorage = () => {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (formData.email === "" || formData.message === "") {
-    alert('Fill please all fields');
-    return;
-  }
-  console.log(formData);
-  localStorage.removeItem('feedback-form-state');
-  form.reset();
-  formData.email = "";
-  formData.message = "";
-});
+// Функція для оновлення об'єкту formData і збереження даних в локальне сховище
+const handleInput = (event) => {
+    formData[event.target.name] = event.target.value.trim();
+    saveToLocalStorage();
+};
 
-window.addEventListener('load', () => {
-  const savedData = localStorage.getItem('feedback-form-state');
-  if (savedData) {
-    const parsedData = JSON.parse(savedData);
-    form.email.value = parsedData.email;
-    form.message.value = parsedData.message;
-    formData.email = parsedData.email;
-    formData.message = parsedData.message;
-  }
-});
+// Функція для відтворення данних з локального сховища
+const loadFromLocalStorage = () => {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        Object.assign(formData, JSON.parse(savedData));
+        form.email.value = formData.email;
+        form.message.value = formData.message;
+    }
+};
+
+// Функція для обробки відправки форми
+const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert('Fill please all fields');
+        return;
+    }
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+    formData.email = "";
+    formData.message = "";
+};
+
+// Додавання слухачів подій
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+// Відновлення даних з локального сховища при завантаженні сторінки
+loadFromLocalStorage();
